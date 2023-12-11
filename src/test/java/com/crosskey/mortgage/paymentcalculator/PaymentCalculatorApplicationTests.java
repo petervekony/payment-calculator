fe19f955cbfc11c1061b9326c96195079416a36d
Péter Vékony <peter.vekony@protonmail.com>
2023-12-11 20:27:19 +0200

    initial commit

---
+++ src/test/java/com/crosskey/mortgage/paymentcalculator/PaymentCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crosskey.mortgage.paymentcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
